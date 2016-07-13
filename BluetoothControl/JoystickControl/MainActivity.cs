0a4f4d43a4ea76473ecf58146917049030cb1aac
sguertl <sebastian.guertl@gmail.com>
2016-07-13 08:37:40 +0200

    First joystick project

---
+++ BluetoothControl/JoystickControl/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace JoystickControl
+{
+    [Activity(Label = "JoystickControl", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen", 
+        ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape, MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.Main);
+            SetContentView(new OverallView(this));
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
+            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
---
+++ BluetoothControl/JoystickControl/MyOvalShape.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics.Drawables;
+using Android.Graphics;
+using Android.Graphics.Drawables.Shapes;
+
+namespace JoystickControl
+{
+    public class MyOvalShape : View
+    {
+        private readonly ShapeDrawable _shape;
+
+        public MyOvalShape(Context context) : base(context)
+        {
+            SetBackgroundColor(Color.White);
+
+            var paint = new Paint();
+            paint.SetARGB(255, 200, 255, 0);
+            paint.SetStyle(Paint.Style.Stroke);
+            paint.StrokeWidth = 4;
+
+            _shape = new ShapeDrawable(new OvalShape());
+            _shape.Paint.Set(paint);
+
+            _shape.SetBounds(20, 20, 300, 200);
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            _shape.Draw(canvas);
+        }
+    }
+}
